4287dc30b647e4a5dc3fd7732083f55f58321588
KemelbekovDaniyar <kemelbekovdaniyar0@gmail.com>
2023-12-14 22:54:49 +0600

    add home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import './styles/main.css'
+
+import Navbar from './components/NavBar/Navbar'
+import Header from './components/Header/Header'
+import Section from './components/Section/Section'
+import Footer from './components/Footer/Footer'
 
 function App() {
-  return (
-    <div className="App">
-     <h1>hello world</h1>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Navbar />
+
+			<Header />
+
+			<Section />
+
+			<Footer />
+		</div>
+	)
 }
 
-export default App;
+export default App
