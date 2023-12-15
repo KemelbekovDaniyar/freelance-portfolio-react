72b4587e218f3386d6c430de77a2bf7415978fe3
KemelbekovDaniyar <kemelbekovdaniyar0@gmail.com>
2023-12-15 14:39:44 +0600

    add Routers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import './styles/main.css'
+import './styles/main.css';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/NavBar/Navbar'
 import Footer from './components/Footer/Footer'
@@ -10,13 +12,18 @@ import Project from './pages/Project'
 function App() {
 	return (
 		<div className='App'>
-			<Navbar />
-			{/* <Home /> */}
-			{/* <Projects /> */}
-			{/* <Contacts /> */}
+			<Router>
+				<Navbar />
 
-			<Project />
-			<Footer />
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/projects' element={<Projects />} />
+					<Route path='/project' element={<Project />} />
+					<Route path='/contacts' element={<Contacts />} />
+				</Routes>
+
+				<Footer />
+			</Router>
 		</div>
 	)
 }
